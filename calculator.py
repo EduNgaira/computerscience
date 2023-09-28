f1d1d4861de3ac976f07b8d36767058ad9244f85
EduNgaira <ngairaedwin@gmail.com>
2023-09-28 11:36:25 -0700

    Edited loop

--- calculator.py
+++ calculator.py
@@ -3,23 +3,24 @@
 """A simple claculator in python"""
 
 def calculator():
-	a, operator, c =input("Enter numbers: ").split()
+	while True:
+		a, operator, c =input("Enter numbers: ").split()
 
-	a = int(a)
-	c = int(c)
+		a = int(a)
+		c = int(c)
 
-	if operator == "+":
-		print("{}".format(a+c))
-	elif operator == "-":
-		print("{}".format(a-c))
-	elif operator == "/":
-		print("{}".format(a/c))
-	elif operator == "x":
-		print("{}".format(a*c))
-	elif operator == "**":
-		print("{}".format(a**c))
-	else:
-		print("Use appropriate symbols")
+		if operator == "+":
+			print("{}".format(a+c))
+		elif operator == "-":
+			print("{}".format(a-c))
+		elif operator == "/":
+			print("{}".format(a/c))
+		elif operator == "x":
+			print("{}".format(a*c))
+		elif operator == "**":
+			print("{}".format(a**c))
+		else:
+			print("Use appropriate symbols")
 
 if __name__ == "__main__":
 	calculator()
